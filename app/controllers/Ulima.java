395d46ab6234514b6a0ffe448e5a0b23e9c650ee
Daniel Flores <dnielfs@gmail.com>
2012-06-19 02:17:27 -0500

    Por fin funciona la conexión a miUlima

--- app/controllers/Ulima.java
+++ app/controllers/Ulima.java
@@ -1,107 +1,97 @@
 package controllers;
 
+/*
+ * Author : Daniel Flores
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.List;
-import java.util.Map;
 
 public class Ulima {
 	
-
-	public static void main(String[] args){
-		// System.out.println(":o");
-
-		HttpURLConnection con;
-		
-		System.out.println("Empezar request ...");
-		
+	public static void login(String codigo, String password){
 		try {
-			//CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
-			
-			// Establecer conexión
-			con = (HttpURLConnection) new URL("https://webaloe.ulima.edu.pe/portalUL/j_security_check").openConnection();
-			// Habilitar redirección
-			//con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			con.setInstanceFollowRedirects(false);
-			// Método de solicitud, POST
-			con.setRequestMethod("POST");
-			// Obtener el stream de la conexión
-			con.setAllowUserInteraction(false);
-			con.setDoInput(true);
-			con.setDoOutput(true);
-			// Usuario :
-			String codigo = "20082219";
-			// Contraseña : 
-			String password = "DFYAPL";
-			String query = "j_username=" + codigo +"&j_password=" + password + "&ac=";			
-		    OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
-		    wr.write("j_username=20082219&j_password=DFYAPL&ac=");
-		    wr.flush();
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			System.out.println(con.getHeaderField("Cookie"));
-			String decodedString;
-			while ((decodedString = in.readLine()) != null) {
-				System.out.println(decodedString);
-			}
-			
-			// LEER COOKIES
-			System.out.println("LEER COOKIES");
 			
 			/*
-			 * Redireccionar
+			 * This cookie manager is important for save cookies through the request
 			 */
 			
-			// MOSTRAR COOKIES
+			CookieManager manager = new CookieManager();
+			manager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+			CookieHandler.setDefault(manager);
 			
-			List<String> cookies = con.getHeaderFields().get("Set-Cookie");
-			for (String cookie : cookies) {
-				
-				int s = cookie.indexOf("; Path=/");
-				cookie = cookie.substring(0,s);
-				System.out.println("Cookie --> " + cookie);
-			}
-				
-			/*
-			// Establecer conexión
-			HttpURLConnection con2 = (HttpURLConnection) new URL("https://webaloe.ulima.edu.pe/portalUL/layout.jsp?tab=1").openConnection();
-			con2.setRequestMethod("GET");
-			con2.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1");
+			System.setProperty("http.keepAlive", "false");
+			String url = "https://webaloe.ulima.edu.pe/portalUL/j_security_check";
+			String charset = "UTF-8";
+			String param1 = codigo;
+			String param2 = password;
+			String query = String.format("j_username=%s&j_password=%s",
+					URLEncoder.encode(param1,charset),
+					URLEncoder.encode(param2,charset));
 			
-			for (String cookie : cookies) {
-				
-				int s = cookie.indexOf("; Path=/");
-				cookie = cookie.substring(0,s);
-				System.out.println("Cookie --> " + cookie);
-				con2.setRequestProperty("Cookie", cookie);
-			}
+			HttpURLConnection connection = (HttpURLConnection)new URL(url).openConnection();
+			connection.setRequestMethod("POST");
+			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.3) Gecko/20100401");
+			connection.setDoOutput(true);
+			connection.setInstanceFollowRedirects(true);
+			connection.setRequestProperty("Accept-Charset",charset);
+			connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset=" + charset);
+			OutputStream output = null;
 			
-			System.out.println(con2.getHeaderField("User-Agent"));*/
-			
-			/*System.out.println(con2.getHeaderFields().toString());
-			BufferedReader in = new BufferedReader(new InputStreamReader(con2.getInputStream()));
-			String decodedString;
-			while ((decodedString = in.readLine()) != null) {
-				System.out.println(decodedString);
+			try{
+				output = connection.getOutputStream();
+				output.write(query.getBytes(charset));
+			}finally{
+				if(output != null) try { output.close(); } catch(IOException logOrIgnore){
+					System.out.println("FAIL");
+				}
 			}
 			
-			con2.disconnect();
-			in.close();	*/
+			// Make request
+			connection.connect();
+			
+			List<String> cookies = connection.getHeaderFields().get("Set-Cookie");
+			
+			System.out.println("COOKIES --> " + cookies);
+			
+			connection.disconnect();
+			
+			// The cookies for session has been saved by the cookie manager.
+			URLConnection connection2 = new URL("http://webaloe.ulima.edu.pe/portalUL/layout.jsp").openConnection();
+			connection2.setRequestProperty("Accept-Charset", charset);
+			connection2.setDoInput(true);
+			connection2.setDoOutput(true);
+			
+			// Read and show response
+			
+		    BufferedReader reader = null;
+		    try {
+		        reader = new BufferedReader(new InputStreamReader(connection2.getInputStream(), charset));
+		        for (String line; (line = reader.readLine()) != null;) {
+		            System.out.println(line);
+		        }
+		    } finally {
+		        if (reader != null) try { reader.close(); } catch (IOException logOrIgnore) {}
+		    }
 			
 		}catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	}
+
+	public static void main(String[] args){
+		System.out.println("Empezar request ...");
+		login("20082219","XXXXX");
 	}
 
 }
