2bf23a1221ff8b290d6b8092ba191327d3c1cfd4
mitsuosh <mitsuo_1991@hotmail.com>
2012-08-15 18:04:25 -0500

    Creando Usuarios

---
+++ app/controllers/Facultades.java
@@ -0,0 +1,94 @@
+package controllers;
+
+import java.util.List;
+import java.util.Map;
+
+import models.Facultad;
+import models.Mensaje;
+import models.Usuario;
+
+import org.codehaus.jackson.JsonNode;
+import org.h2.expression.ExpressionList;
+
+import play.*;
+import play.data.*;
+import play.mvc.*;
+import play.mvc.Http.RequestBody;
+
+import models.*;
+
+public class Facultades extends Controller {
+
+	static Form<Facultad> facultadForm = form(Facultad.class);
+
+	/*
+	 * Elegir acción
+	 */
+
+	@BodyParser.Of(play.mvc.BodyParser.Json.class)
+	public static Result nuevoMensaje(String id)
+	{
+		System.out.println("EN FACULTADES : NUEVO MENSAJE");
+		
+		RequestBody body = request().body();
+		
+		System.out.println(body.toString());
+		
+		JsonNode json = body.asJson();
+		
+		System.out.println(json.toString());
+		
+		String uid = json.get("id").toString();
+		String contenido = json.get("mensaje").toString();
+		
+		/*
+		 * Obtener facultad
+		 */
+		
+		Facultad facultad = Facultad.find.byId(Long.parseLong(id));
+		
+		/*
+		 * Crear mensaje
+		 */
+		
+		Mensaje mensaje = new Mensaje();
+		mensaje.setEmisor(Usuario.getUser(Long.parseLong(id)));
+		mensaje.setContenido(contenido);
+		mensaje.setFacultad(facultad);
+		mensaje.save();
+		
+		return ok(json.toString());		
+	}
+
+	@BodyParser.Of(play.mvc.BodyParser.Json.class)
+	public static Result create(){
+
+		System.out.println("EN FACULTADES : NUEVO MENSAJE");
+		
+		RequestBody body = request().body();
+		
+		System.out.println(body.toString());
+		
+		JsonNode json = body.asJson();
+		
+		System.out.println(json.toString());
+		
+		String nombre = json.get("facultad.nombre").toString();
+
+		String universidad_id = json.get("facultad.universidad").toString();
+		
+		/*
+		 * Obtener facultad
+		 */
+		
+		Universidad universidad = Universidad.find.byId(Long.parseLong(universidad_id));
+
+		Facultad facultad = new Facultad();
+		facultad.setNombre(nombre);
+		facultad.setUniversidad(universidad);
+		facultad.save();
+
+		return ok("");
+	}
+
+}
